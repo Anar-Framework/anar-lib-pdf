12203a505d48e652fb62f881a72054e5bebe8101
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-15 14:25:05 +0430

    PDF Utility library of Anar Framework

---
+++ src/main/java/af/gov/anar/lib/pdf/AnarLibPdfApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibPdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/lib/pdf/AnarLibPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
